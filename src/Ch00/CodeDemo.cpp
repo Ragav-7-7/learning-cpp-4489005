b43785afec97f64fe01f708144bc2c8049705002
Ragav-7-7 <168485692+Ragav-7-7@users.noreply.github.com>
2024-04-29 21:54:39 +0000

    added a variable

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 int main(){
-    float num_1, num_2, result;
+    float num_1, num_2, result, num_3;
 
     std::cout << "Enter number 1: " << std::flush;
     std::cin >> num_1;
